49207bf9344b1dc8c86cb2fc9495fa87a69a12bd
Rick Russell <rick@rickrussell.org>
2014-06-10 10:19:28 -0400

    Added support for deleting submitted questions as well as some fixes for the screen display code.

--- client/admin.js
+++ client/admin.js
@@ -1,11 +1,11 @@
 // This is a main page for the application.
 
 Template.incomming.smsMessages = function () {
-  return Messages.find({Approved: false}, {sort: {CreatedAt: 1}});
+  return Messages.find({Approved: false, DeletedAt: {$exists: false}}, {sort: {CreatedAt: 1}});
 };
 
 Template.approved.approvedMessages = function () {
-  return Messages.find({Approved: true}, {sort: {CreatedAt: 1}});
+  return Messages.find({Approved: true, DeletedAt: {$exists: false}}, {sort: {CreatedAt: 1}});
 };
 
 Template.eachMessage.events({
@@ -16,6 +16,20 @@ Template.eachMessage.events({
     Messages.update(this._id, {$set: {Approved: false}});
   },
   'click .selectedMessage': function () {
-    Messages.updateSelected(this._id);
+    if(this.Selected) {
+      Messages.clearSelection(this._id);
+    } else {
+      Messages.updateSelected(this._id);
+    }
+  }
+});
+
+Template.eachIncommingMessage.events({
+  'click .approveMessage': function () {
+    Messages.update(this._id, {$set: {Approved: true}});
+  },
+  'click .deleteMessage': function () {
+    var now = new Date();
+    Messages.update(this._id, {$set: {DeletedAt: now.valueOf()}});
   }
 });
